8ff931132f5ef21730fc872df04cfc5cdd3d9dd1
Veda Kothadia <kothadiaveda@gmail.com>
2025-06-22 18:07:40 -0400

    Modified js content

---
+++ app.js
@@ -0,0 +1,10 @@
+const cursor = document.querySelector(".circle-cursor");
+      const shadow = document.querySelector(".cursor-shadow");
+
+      document.addEventListener("mousemove", (e) => {
+        const x = e.clientX;
+        const y = e.clientY;
+
+        cursor.style.transform = `translate(${x}px, ${y}px)`;
+        shadow.style.transform = `translate(${x}px, ${y}px)`;
+      });
